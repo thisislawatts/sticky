4d90fa1bae53d713df10f739305638a3a5b12bd5
LA Watts <luke@thisis.la>
2016-04-12 12:35:12 +0100

    Fixes jslint errors

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -1,8 +1,8 @@
 ;/**
  *
  *
- *	
- * 
+ *
+ *
  *  Example Usage:
  *  <div data-sticky></div>
  *  <div data-sticky data-sticky-full-width></div>
@@ -12,7 +12,7 @@
 (function( $, Modernizr, undefined ) {
 	var Sticky = function ($el) {
 		var _this = this;
-		
+
 		_this.options = {
 			full_width: $el.data('sticky-full-width') === '',
 			fixed: $el.data('sticky-fixed') === ''
@@ -22,7 +22,7 @@
 
 		if ( window.innerWidth > 880 && !Modernizr.touch )
 			_this.init();
-		
+
 		if (!window.Stickies) window.Stickies = [];
 
 		Stickies.push( _this );
@@ -33,7 +33,7 @@
 	Sticky.prototype.init = function() {
 		var _this = this;
 
-		_this.sticky_id = 'sticky-item-' + Math.round( Math.random() * 100 ),
+		_this.sticky_id = 'sticky-item-' + Math.round( Math.random() * 100 );
 		_this.$wrap = $('<div id="'+ _this.sticky_id +'" class="sticky--wrapper"></div>');
 		_this.$wrap.css( _this.getWrapperStyles() );
 
@@ -44,7 +44,7 @@
 		console.log("Options: ", _this.options);
 		console.log("Offset Top: ", _this.offsetTop);
 		_this.offsetLeft = this.$wrap.offset().left + this.$el.offset().left;
-	}
+	};
 
 
 	/* http://stackoverflow.com/questions/13382516/getting-scroll-bar-width-using-javascript */
@@ -71,11 +71,11 @@
 	    outer.parentNode.removeChild(outer);
 
 	    return widthNoScroll - widthWithScroll;
-	}
+	};
 
 	Sticky.prototype.getWrapperStyles = function() {
 
-		var _this = this, 
+		var _this = this,
 			position = (_this.$el.css('position') === "absolute" ) ? 'absolute' : 'relative',
 			styles = {};
 
@@ -88,7 +88,7 @@
 				if ( val !== "auto" ) {
 					styles[prop] = val;
 				}
-			};
+			}
 
 			styles.width = _this.$el.outerWidth() + 5;
 		}
@@ -98,7 +98,7 @@
 			'position': position,
 			'width' : _this.$el.outerWidth()
 		}, styles );
-	}
+	};
 
 	Sticky.prototype.getElementStyle = function() {
 
@@ -107,25 +107,25 @@
 			'position': 'fixed',
 			'top' : _this.options.fixed ? _this.offsetTop : 0,
 			'width': _this.$el.outerWidth()
-		}
+		};
 
 		if ( _this.options.full_width ) {
-			styles['width'] = window.innerWidth - _this.getScrollbarWidth();
+			styles.width = window.innerWidth - _this.getScrollbarWidth();
 
 		} else {
 			styles = $.extend(styles, {
 				'left': _this.offsetLeft,
-			})
+			});
 		}
 
-		return styles
-	}
+		return styles;
+	};
 
 	Sticky.prototype.checkPosition = function( offset_y ) {
 		var _this = this;
 		console.log(_this.offsetTop, offset_y);
 		if ( _this.triggerOffsetTop < offset_y ) {
-			
+
 			_this.$el.addClass('s__stuck').css(
 				_this.getElementStyle() );
 
@@ -137,14 +137,14 @@
 				'width': _this.$el.outerWidth()
 			});
 		}
-	}
+	};
 
 	$('[data-sticky]').each(function() {
 		new Sticky( $(this) );
-	})
+	});
 
 	// We've got a cheeky wrapper element capturing scrolls
-	// 
+	//
 	var $page = $('#page').length ? $('#page') : $(window);
 
 	if ( window.Stickies ) {
@@ -152,9 +152,8 @@
 			var scrollTop = $page.scrollTop();
 			for (var i = window.Stickies.length - 1; i >= 0; i--) {
 				window.Stickies[i].checkPosition( scrollTop );
-			};
+			}
 		});
-
 	}
 
 	console.log("jquery.sticky.v2");
